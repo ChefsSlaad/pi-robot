999bf26898557aa1188f35e8b7552bc2eac7fddb
marcwagner <wagner.marc@gmail.com>
2017-02-03 13:02:33 +0100

    Update and rename rijden_stap1.py to rijden_afstand_besturen.py

--- les4/rijden_afstand_besturen.py
+++ les4/rijden_afstand_besturen.py
@@ -10,12 +10,16 @@ def main(scherm):
         key = scherm.getkey()
         if key == 'KEY_UP':
             richting = 'vooruit'
+            # voeg hier de code in om de auto vooruit te laten bewegen
         elif key == 'KEY_LEFT':
             richting = 'links'
+            # voeg hier de code in om de auto naar links te laten bewegen
         elif key == 'KEY_DOWN':
             richting = 'achteruit'
+            # voeg hier de code in om de auto achteruit te laten bewegen
         elif key == 'KEY_RIGHT':
             richting = 'rechts'
+            # voeg hier de code in om de auto naar rechts te laten bewegen
         elif key =='q':
             run = False
         else:
@@ -23,7 +27,7 @@ def main(scherm):
         scherm.addstr(r,0, richting)
         time.sleep(0.01)
         r=r+1
-        # als r >40 en het scherm is kleiner,dan een error
+        # als r >20 en het scherm is kleiner,dan een error
         if r > 20:  
             scherm.clear()
             r = 0
